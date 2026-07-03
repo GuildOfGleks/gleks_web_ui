cb13d0102945f078ba05873447248fd4622c9e43
Roman Malitskyi <chebureck77@gmail.com>
2026-07-03 21:49:54 +0300

    showcase page

--- projects/ui-showcase/src/app/app.spec.ts
+++ projects/ui-showcase/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ui-showcase');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Gleks UI Showcase');
   });
 });
