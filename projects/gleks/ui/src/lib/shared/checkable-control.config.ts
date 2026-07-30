050097756a66c6c2a7a44206bc1681f54f1945a9
Маліцький Роман Петрович <su23719@sunpp.local>
2026-07-30 14:15:58 +0300

    do more compact sizes

--- projects/gleks/ui/src/lib/shared/checkable-control.config.ts
+++ projects/gleks/ui/src/lib/shared/checkable-control.config.ts
@@ -10,18 +10,18 @@ export const GOG_CHECKABLE_CONTROL_PADDING = 'var(--control-checkbox-padding, 6p
 
 export const GOG_CHECKABLE_CONTROL_SIZE_MAP = {
   sm: {
-    boxSize: 'var(--control-checkbox-box-size-sm, 18px)',
-    labelSize: 'var(--control-checkbox-label-size-sm, 0.8125rem)',
-    indicatorSize: 'var(--control-checkbox-icon-size-sm, 12px)',
+    boxSize: 'var(--control-checkbox-box-size-sm, 12px)',
+    labelSize: 'var(--control-checkbox-label-size-sm, 0.6875rem)',
+    indicatorSize: 'var(--control-checkbox-icon-size-sm, 10px)',
   },
   md: {
-    boxSize: 'var(--control-checkbox-box-size-md, 24px)',
-    labelSize: 'var(--control-checkbox-label-size-md, 0.9375rem)',
-    indicatorSize: 'var(--control-checkbox-icon-size-md, 14px)',
+    boxSize: 'var(--control-checkbox-box-size-md, 18px)',
+    labelSize: 'var(--control-checkbox-label-size-md, 0.8125rem)',
+    indicatorSize: 'var(--control-checkbox-icon-size-md, 12px)',
   },
   lg: {
-    boxSize: 'var(--control-checkbox-box-size-lg, 32px)',
-    labelSize: 'var(--control-checkbox-label-size-lg, 1.0625rem)',
-    indicatorSize: 'var(--control-checkbox-icon-size-lg, 18px)',
+    boxSize: 'var(--control-checkbox-box-size-lg, 24px)',
+    labelSize: 'var(--control-checkbox-label-size-lg, 0.9375rem)',
+    indicatorSize: 'var(--control-checkbox-icon-size-lg, 14px)',
   },
 } as const satisfies Record<GogSize, GogCheckableControlSizeConfig>;
